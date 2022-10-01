4b397758c1a48dae9364301c8c9b03fb7fa4edad
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2022-10-01 15:11:13 -0400

    Changed timer 00 to 10 so it looks like 10 ... 2, 1 and not 9 ... 2, 1, 0

--- src/ui/Countdown.gd
+++ src/ui/Countdown.gd
@@ -5,7 +5,7 @@ var countdown = 10
 func _process(delta):
 	countdown -= delta
 	
-	if (countdown <= 0):
+	if (countdown <= 1):
 		countdown = 10 + countdown
 	
 	var displayTime = "%02d" % [fmod(countdown, 60)]
