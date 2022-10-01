718789d1598e100d29142b1218c51550dd4ccc48
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2022-10-01 12:03:02 -0400

    Fixed back button

--- src/ui/BackBtn.gd
+++ src/ui/BackBtn.gd
@@ -2,3 +2,7 @@ extends Button
 
 func _on_BackBtn_pressed():
 	var _trash = get_tree().change_scene("res://scn/UI/mainMenu.tscn")
+
+
+func _on_BackBtn2_pressed():
+	pass # Replace with function body.
