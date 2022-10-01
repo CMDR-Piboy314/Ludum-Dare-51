7b5435bedc7a7b3c8729d6966f5c6ded6d0aea05
CMDR-Piboy314 <92357316+CMDR-Piboy314@users.noreply.github.com>
2022-10-01 10:59:16 -0400

    FIX!

--- scn/UI/Buttons/Buttons.gd
+++ scn/UI/Buttons/Buttons.gd
@@ -3,11 +3,11 @@ extends VBoxContainer
 export(PackedScene) onready var play_scn
 export(PackedScene) onready var stng_scn
 
-func _on_NewGameBtn_pressed():
-	var trash = get_tree().change_scene(play_scn)
+func _on_PlayBtn_pressed():
+	var _trash = get_tree().change_scene_to(play_scn)
 
 func _on_SettingsBtn_pressed():
-	var trash = get_tree().change_scene(stng_scn)
+	var _trash = get_tree().change_scene_to(stng_scn)
 
 func _on_QuitBtn_pressed():
 	get_tree().quit()
