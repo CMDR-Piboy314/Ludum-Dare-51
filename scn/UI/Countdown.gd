0047adc69ec99897b7ff1321ed3849027be57f32
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2022-09-30 20:21:05 -0400

    Countdown timer resets at 0

--- scn/UI/Countdown.gd
+++ scn/UI/Countdown.gd
@@ -16,6 +16,9 @@ func _ready():
 func _process(delta):
 	countdown -= delta
 	
+	if (countdown <= 0):
+		countdown = 10 + countdown
+	
 	var displayTime = "%02d" % [fmod(countdown, 60)]
 	text = displayTime
 	
