6856442da7f60fe7c14c0432693487879fcd2a39
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2022-10-01 12:10:01 -0400

    Back button now works

--- src/ui/BackBtn.gd
+++ src/ui/BackBtn.gd
@@ -2,7 +2,3 @@ extends Button
 
 func _on_BackBtn_pressed():
 	var _trash = get_tree().change_scene("res://scn/UI/mainMenu.tscn")
-
-
-func _on_BackBtn2_pressed():
-	pass # Replace with function body.
