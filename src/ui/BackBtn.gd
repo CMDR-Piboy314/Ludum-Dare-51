6248b6ae809af5826593a7556c7d8e24a9005b79
CMDR-Piboy314 <92357316+CMDR-Piboy314@users.noreply.github.com>
2022-10-01 11:58:15 -0400

    FIX!

--- src/ui/BackBtn.gd
+++ src/ui/BackBtn.gd
@@ -1,4 +1,4 @@
 extends Button
 
 func _on_BackBtn_pressed():
-	var _trash = get_tree().change_scene("res://scn/UI/TitleScreen.tscn")
+	var _trash = get_tree().change_scene("res://scn/UI/mainMenu.tscn")
