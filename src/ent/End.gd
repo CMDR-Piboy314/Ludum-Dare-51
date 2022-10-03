376e8b42385cc6321ebd2acffe4551fb2be238a4
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2022-10-03 09:42:37 -0400

    Finish end script

--- src/ent/End.gd
+++ src/ent/End.gd
@@ -3,6 +3,9 @@ extends Area2D
 # TODO: Write end function (Alex)
 # It should save your time, bring you back to level select etc.
 func end():
+	globals.playerHealth = 100
+	globals.playerInventory = globals.inventoryItems.PISTOL
+	var _trash = get_tree().change_scene("res://scn/UI/levelSelect.tscn")
 	pass
 
 func _on_End_area_entered(area):
